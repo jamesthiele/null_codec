14b9f58e4c64e643fa847cc3c268db0ae8df426f
jamesthiele <james.thiele@gmail.com>
2016-09-17 00:01:16 +0000

    hanged variable name.  Added 'outputSrc' variable Made minor formatting changes.

--- acodec.py
+++ acodec.py
@@ -4,20 +4,23 @@ import encodings, codecs, re, sys
 # Our StreamReader
 class aStreamReader(codecs.StreamReader):
     def readline(self, size=None, keepends=True):
-        r = self.stream.read().decode("utf8")
-        return  r
+        inputSrc  = self.stream.read().decode("utf8")
+        outputSrc = inputSrc  ## TODO Placeholder
+        return outputSrc
         ## return  u'%s\n'%self.pysrc.pop(0) if self.pysrc else u''
         
 def search_function(s):
     if s!="acodec": 
         return None
-    u8=encodings.search_function("utf8")
-    return codecs.CodecInfo( name='acodec', 
-        encode=u8.encode, decode=u8.decode,
-        incrementalencoder=u8.incrementalencoder,
-        incrementaldecoder=u8.incrementaldecoder,
-        streamreader=aStreamReader,        # acodec StreamReader
-        streamwriter=u8.streamwriter)
+
+    u8 = encodings.search_function("utf8")
+    return codecs.CodecInfo( name = 'acodec', 
+                             encode = u8.encode,
+                             decode = u8.decode,
+                             incrementalencoder = u8.incrementalencoder,
+                             incrementaldecoder = u8.incrementaldecoder,
+                             streamreader = aStreamReader,        # acodec StreamReader
+                             streamwriter = u8.streamwriter)
 
 codecs.register(search_function)  # register our new codec search function
 # End of acodec.py
