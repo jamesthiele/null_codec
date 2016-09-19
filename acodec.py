f0c3c0ddc74301ad94a84d55090df559a74ae707
jamesthiele <james.thiele@gmail.com>
2016-09-19 19:57:04 +0000

    Use StringIO and readline().

--- acodec.py
+++ acodec.py
@@ -3,26 +3,29 @@ import encodings, codecs, re, sys
 
 # Our StreamReader
 class aStreamReader(codecs.StreamReader):
+    outputSrc = None
+
     def outputFromInput(input):
         return input
         
     def readline(self, size=None, keepends=True):
-        inputSrc  = self.stream.read().decode("utf8")
-        outputSrc = outputFromInput(inputSrc)
-        return outputSrc
+        if outputSrc == None:
+            inputSrc  = self.stream.read().decode("utf8")
+            outputSrc = StringIO(outputFromInput(inputSrc))
+        return outputSrc.readline()
 
-def search_function(s):
-    if s!="acodec": 
-        return None
+    def search_function(s):
+        if s!="acodec": 
+            return None
 
-    u8 = encodings.search_function("utf8")
-    return codecs.CodecInfo( name = 'acodec', 
-                             encode = u8.encode,
-                             decode = u8.decode,
-                             incrementalencoder = u8.incrementalencoder,
-                             incrementaldecoder = u8.incrementaldecoder,
-                             streamreader = aStreamReader,        # acodec StreamReader
-                             streamwriter = u8.streamwriter)
+        u8 = encodings.search_function("utf8")
+        return codecs.CodecInfo( name = 'acodec', 
+                                 encode = u8.encode,
+                                 decode = u8.decode,
+                                 incrementalencoder = u8.incrementalencoder,
+                                 incrementaldecoder = u8.incrementaldecoder,
+                                 streamreader = aStreamReader,        # acodec StreamReader
+                                 streamwriter = u8.streamwriter)
 
-codecs.register(search_function)  # register our new codec search function
+codecs.register(aStreamReader.search_function)  # register our new codec search function
 # End of acodec.py
