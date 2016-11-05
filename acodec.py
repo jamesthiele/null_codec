3c3ef960d9db7e380f25cf9cc812f0b4cd45dc41
EC2 Default User <ec2-user@ip-172-31-22-45.us-west-2.compute.internal>
2016-11-05 20:39:20 +0000

    Moved search_function() into class aStreamReader -- issue #4

--- acodec.py
+++ acodec.py
@@ -12,18 +12,18 @@ class aStreamReader(codecs.StreamReader):
         outputSrc = outputFromInput(inputSrc)
         return outputSrc
 
-def search_function(s):
-    if s!= codec_name: 
-        return None
+    def search_function(s):
+        if s!= codec_name: 
+            return None
 
-    u8 = encodings.search_function("utf8")
-    return codecs.CodecInfo( name = codec_name, 
-                             encode = u8.encode,
-                             decode = u8.decode,
-                             incrementalencoder = u8.incrementalencoder,
-                             incrementaldecoder = u8.incrementaldecoder,
-                             streamreader = aStreamReader,        # null_codec StreamReader
-                             streamwriter = u8.streamwriter)
+        u8 = encodings.search_function("utf8")
+        return codecs.CodecInfo( name = codec_name, 
+                                 encode = u8.encode,
+                                 decode = u8.decode,
+                                 incrementalencoder = u8.incrementalencoder,
+                                 incrementaldecoder = u8.incrementaldecoder,
+                                 streamreader = aStreamReader,        # null_codec StreamReader
+                                 streamwriter = u8.streamwriter)
 
-codecs.register(search_function)  # register our new codec search function
+codecs.register(aStreamReader.search_function)  # register our new codec search function
 # End of acodec.py
