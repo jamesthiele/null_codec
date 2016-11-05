316a112ec253928caae1b20a707abd6b42d458da
EC2 Default User <ec2-user@ip-172-31-22-45.us-west-2.compute.internal>
2016-11-05 20:30:18 +0000

    Added test for whether incorrect codec name causes error -- issue #6

---
+++ bad_codec_name_test.py
@@ -0,0 +1,4 @@
+# atest.py (written in our new language A)
+# encoding: bac_codec_name
+print "yo dude"
+# End of atest.py
--- runtest.py
+++ runtest.py
@@ -1,3 +1,8 @@
 import acodec
 import atest
+try:
+    import bad_codec_name_test
+    print "Failed test for whether incorrect codec name causes error #6"
+except SyntaxError:
+    print "Passed test for whether incorrect codec name causes error #6"
 
